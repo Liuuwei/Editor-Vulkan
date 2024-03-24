3a684c32486756ccafe7dc49166e14518aafb693
Liuuwei <6iu5ei@gmail.com>
2024-03-25 02:17:16 +0800

    add moveLimit in adjustCursor

--- src/LineNumber.cpp
+++ src/LineNumber.cpp
@@ -1,18 +1,22 @@
 #include "LineNumber.h"
 #include "Editor.h"
 #include "glm/fwd.hpp"
+#include <iostream>
 
 LineNumber::LineNumber(const Editor& editor) : Editor(editor.screen_.x, editor.screen_.y, editor.lineHeight_) {
+    lines_.clear();
     lines_.push_back({});
     addLineNumber(lines_.back(), lines_.size());
+    // std::cout << lines_.back() << std::endl;
     wordCount_ = lines_.size() * 5;
 }
 
 void LineNumber::adjustCursor(glm::ivec2 cursorPos, const std::vector<std::string>& lines) {
     cursorPos_ = cursorPos;
+    std::cout << lines_.size() << std::endl;
     while (lines_.size() < lines.size()) {
         lines_.push_back({});
-        addLineNumber(lines_.back(), lines_.size() + 1);
+        addLineNumber(lines_.back(), lines_.size());
     }
 
     while (lines_.size() > lines.size()) {
@@ -20,6 +24,8 @@ void LineNumber::adjustCursor(glm::ivec2 cursorPos, const std::vector<std::strin
     }
 
     wordCount_ = lines_.size() * 5;
+
+    moveLimit();
 }
 
 void LineNumber::addLineNumber(std::string& line, int32_t lineNumber) {
